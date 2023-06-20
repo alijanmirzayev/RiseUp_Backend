c3d1690536f21dc729916576ba643c4f5bd7e43b
alijanmirzayev <mirzeyevalican1@gmail.com>
2023-06-20 10:52:20 +0400

    added put and delete method

--- routes/category.route.js
+++ routes/category.route.js
@@ -6,5 +6,7 @@ const r = Router();
 r.get('/', Category.getAll);
 r.get('/:id', Category.getById);
 r.post('/', Category.add);
+r.put('/:id', Category.updateById)
+r.delete('/:id', Category.deleteById)
 
 export default r
