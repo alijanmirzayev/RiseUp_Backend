14813657396cf6fb1327847004e0df5c7ba01dbc
alijanmirzayev <mirzeyevalican1@gmail.com>
2023-06-20 10:48:48 +0400

    added put and delete method for author route

--- routes/author.route.js
+++ routes/author.route.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { Author } from '../controllers/author.controller';
+import { Author } from '../controllers/author.controller.js';
 
 const r = Router();
 
 r.get('/', Author.getAll);
 r.get('/:id', Author.getById);
 r.post('/', Author.add);
+r.put('/:id', Author.updateById)
+r.delete('/', Author.deleteById)
 
 export default r
