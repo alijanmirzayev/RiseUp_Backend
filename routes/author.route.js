dea7a277d11f152b26239d29ebc4225168e7e6b9
alijanmirzayev <mirzeyevalican1@gmail.com>
2023-06-20 10:55:47 +0400

    added Middleware

--- routes/author.route.js
+++ routes/author.route.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { Author } from '../controllers/author.controller.js';
+import { authMiddleWare } from '../middleware/authMiddleware.js';
 
 const r = Router();
 
 r.get('/', Author.getAll);
 r.get('/:id', Author.getById);
-r.post('/', Author.add);
-r.put('/:id', Author.updateById)
-r.delete('/:id', Author.deleteById)
+r.post('/', authMiddleWare, Author.add);
+r.put('/:id', authMiddleWare, Author.updateById)
+r.delete('/:id', authMiddleWare, Author.deleteById)
 
 export default r
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { Category } from '../controllers/category.controller.js';
+import { authMiddleWare } from '../middleware/authMiddleware.js';
 
 const r = Router();
 
 r.get('/', Category.getAll);
 r.get('/:id', Category.getById);
-r.post('/', Category.add);
-r.put('/:id', Category.updateById)
-r.delete('/:id', Category.deleteById)
+r.post('/', authMiddleWare, Category.add);
+r.put('/:id', authMiddleWare, Category.updateById)
+r.delete('/:id', authMiddleWare, Category.deleteById)
 
 export default r
