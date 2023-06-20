07bf314b7ad0a9a569772c992ed4bc1a8c48c063
alijanmirzayev <mirzeyevalican1@gmail.com>
2023-06-20 10:52:05 +0400

    added updateById and deleteById for category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -28,4 +28,23 @@ export const Category = {
             return res.status(500).send(error)
         }
     },
+    updateById: async (req, res) => {
+        try {
+            const { id } = req.params
+            const validationData = categoryValidation(req.body)
+            const data = await categoryModel.findByIdAndUpdate(id, validationData)
+            return res.status(201).send(data)
+        } catch (error) {
+            return res.status(500).send(error)
+        }
+    },
+    deleteById: async (req, res) => {
+        try {
+            const { id } = req.params
+            const data = await categoryModel.findByIdAndDelete(id)
+            return res.status(201).send(`${id} Deleted!`)
+        } catch (error) {
+            return res.status(500).send(error)
+        }
+    },
 }
